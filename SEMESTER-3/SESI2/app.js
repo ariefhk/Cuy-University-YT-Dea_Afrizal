12350d9ab703813ef33f178f0bd62bb24c78daeb
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-10-03 07:45:22 +0700

    add params and query

--- SEMESTER-3/SESI2/app.js
+++ SEMESTER-3/SESI2/app.js
@@ -4,3 +4,18 @@ const PORT = 3000;
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.post("/hello", (req, res) => {
+  console.log({ RequestfromOut: req.body });
+  let username = req.query.name;
+  console.log({ username: username });
+  res.send("Hallo");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
